698368af29fc07d7b37dadcd696e14f551113332
Sunpyo Hong <sh4023@nyu.edu>
2021-03-12 02:49:15 +0900

    update

--- src/movies/app.js
+++ src/movies/app.js
@@ -1,6 +1,48 @@
 // app.js
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'hbs');
 
 require('./db.js');
 
 const mongoose = require('mongoose');
+
 const Movie = mongoose.model('Movie');
+
+const path = require('path');
+
+const publicPath = path.resolve(__dirname, "public");
+
+app.use(express.static(publicPath));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/movies', function(req, res) {
+
+    const directorName = req.query.director;
+
+    Movie.find({}, function(err, movies, count) {
+        if (directorName === undefined) {
+            res.render('movies', {movies: movies, condition: false});
+        } else {
+            res.render('movies', {movies: movies.filter(movie => movie.director === directorName), directorName, condition: true});
+        }
+    });
+});
+
+app.get('/movies/add', function(req, res){
+    res.render('add');
+});
+
+app.post('/movies/add', function(req, res) {
+    new Movie({
+        title: req.body.title,
+        director: req.body.director,
+        year: req.body.year
+    }).save(function(err, movies, count){
+        res.redirect('/movies');
+    });
+});
+
+app.listen(3000);
--- src/movies/db.js
+++ src/movies/db.js
@@ -3,3 +3,14 @@ const mongoose = require('mongoose')
 
 
 // my schema goes here!
+
+const Movie = new mongoose.Schema({
+	title: String,
+	director: String,
+    year: Number
+});
+
+mongoose.model('Movie', Movie);
+
+
+mongoose.connect('mongodb://localhost/hw05', {useNewUrlParser: true});
